a8235730004e455ddb0b94092a612ac7b5bc319e
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-06-13 15:41:51 -0300

    Added DataBaseDBContext and update Program

---
+++ EmployeeManagement/Data/DataBaseDBContext.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManagement.Identity;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class DataBaseDBContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
+        public DataBaseDBContext(DbContextOptions<DataBaseDBContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Office> Offices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role).WithMany(r => r.UserRoles)
+                .HasForeignKey(r => r.RoleId).IsRequired();
+
+                userRole.HasOne(ur => ur.User).WithMany(r => r.UserRoles)
+                .HasForeignKey(u => u.UserId).IsRequired();
+            });
+        }
+    }
+}
--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,4 +1,19 @@
+using System.Text;
+using EmployeeManagement.Data;
+using EmployeeManagement.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using EmployeeManagement.AutoMapperHelper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+//Variables
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("EmployeeDb");
+var appSettingsToken = builder.Configuration.GetSection("AppSettings:Token").Value;
+var MyAllowSpecificOrigins = "_myAllowSpecifiOrigins";
 
 // Add services to the container.
 
@@ -6,6 +21,38 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataBaseDBContext>(option => option.UseSqlServer(connectionString));
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy(name: MyAllowSpecificOrigins, builder => { builder.WithOrigins("").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin(); });
+});
+
+IdentityBuilder identityBuilder = builder.Services.AddIdentityCore<User>(opt => opt.SignIn.RequireConfirmedEmail = true);
+identityBuilder = new IdentityBuilder(identityBuilder.UserType, typeof(Role), builder.Services);
+identityBuilder.AddEntityFrameworkStores<DataBaseDBContext>();
+identityBuilder.AddRoleValidator<RoleValidator<Role>>();
+identityBuilder.AddRoleManager<RoleManager<Role>>();
+identityBuilder.AddSignInManager<SignInManager<User>>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSettingsToken)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddMvc(opts =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser().Build();
+    opts.Filters.Add(new AuthorizeFilter(policy));
+});
 
 var app = builder.Build();
 
@@ -18,7 +65,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
